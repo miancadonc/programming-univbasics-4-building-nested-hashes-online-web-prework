5f422181ab7c24ecb5dc58c12a939d100517dddb
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-06 20:14:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_pieces_owned => {},
-	      :three_pieces_owned => {},
-	      :four_pieces_owned => {}
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 75,
+	      :four_pieces_owned => 100
 	    },
 	    :names => {
 	      :names_1 => {},
